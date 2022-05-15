b9c7c099cc756091d58e21c9e4dbc4e34f822543
Sharon Atim <venusatim@gmail.com>
2022-05-15 17:42:17 +0100

    CounterView added

---
+++ lib/counter/view/counter_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../counter.dart';
+import '../cubit/counter_cubit.dart'; //TODO: Add counter.dart page
+
+/// {@template counter_view}
+/// A [StatelessWidget] which reacts to the provided
+/// [CounterCubit] state and notifies it in response to user input.
+/// {@endtemplate}
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        // BlocBuilder is used to wrap the Text widget in order to update the
+        // text anytime the CounterCubit changes.
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state');
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            child: const Icon(Icons.add),
+            // context.read looks up the closest counterCubit instance.
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: Key('counterView_decrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          )
+        ],
+      ),
+    );
+  }
+}
