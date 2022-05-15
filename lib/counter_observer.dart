d03dd45f2423a536a12570f1632efd13f687eba0
Sharon Atim <venusatim@gmail.com>
2022-05-15 15:43:24 +0100

    CounterObserver added to observe state changes

---
+++ lib/counter_observer.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which observes all state changes.
+/// {@endtemplate}
+///
+class CounterObserver extends BlocObserver {
+  @override
+
+  // Called whenever a change occurs in any bloc, when a new state is emitted.
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('${bloc.runtimeType} $change');
+  }
+}
