e866be67553d7a726e42fd4770aa2237ceb5259c
Sharon Atim <venusatim@gmail.com>
2022-05-15 16:07:27 +0100

    CounterApp and home: CounterPage() added

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'counter/counter.dart';
+
+/// {@template counter_app}
+/// A [MaterialApp] which sets the 'home' to [CounterPage]/
+/// {@endtemplate}
+
+class CounterApp extends MaterialApp {
+  const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
+
+  /// {@macro counter_app}
+}
